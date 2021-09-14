09b898419cbd040d84f298f877d339d5a9ff57ab
lms19880914 <lms19880914@qq.com>
2021-09-14 14:13:30 +0800

    增加测试用例类

---
+++ src/test/java/net/csibio/propro/test/algorithm/ConvolutionUtilTest.java
@@ -0,0 +1,18 @@
+package net.csibio.propro.test.algorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ConvolutionUtilTest {
+
+    @Test
+    void testAdaptiveAccumulation() {
+
+    }
+
+    @Test
+    void testAccumulation() {
+
+    }
+}
